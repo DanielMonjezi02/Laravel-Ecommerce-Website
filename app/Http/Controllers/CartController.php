6724d0c66be791ceac421d29182cebcb25b693dd
Daniel Monjezi <U2066800@unimail.hud.ac.uk>
2022-12-04 17:24:24 +0000

    Deleted unused functions in controllers

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -41,51 +41,19 @@ class CartController extends Controller
 
         return redirect()->route('products.index')->with('added', 'Added product to cart');   
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
+    
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Cart  $cart
      * @return \Illuminate\Http\Response
      */
+
     public function show(Cart $cart)
     {
         return view('products.show', ['product' => $cart->product]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Cart  $cart
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Cart $cart)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Cart  $cart
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Cart $cart)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
