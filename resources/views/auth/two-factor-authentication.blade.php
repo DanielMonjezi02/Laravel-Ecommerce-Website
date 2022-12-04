22fd24b369109ccc0eb0ede78bee4fc77c2b597f
Daniel Monjezi <U2066800@unimail.hud.ac.uk>
2022-12-04 16:48:38 +0000

    Added errors handlers in 2FA/Login and Recovery page

--- resources/views/auth/two-factor-authentication.blade.php
+++ resources/views/auth/two-factor-authentication.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.app')
 @section('title', 'Login')
 
-@if (Auth::guest()) 
-    <div class="userForm">
-        <form action="{{ url('/two-factor-challenge') }}" method="POST">
-            @csrf
-            <label for ="code">Two Factor Code:</label>
-            <input type="text" name="code" id="code"/>
-            <p style="font-size: 14px;"><a href="{{ route('recoveryLogin') }}">Don't have access to authenticator?</a></p>
-            <button type="submit" class="btn btn-blue">Submit</button>
-        </form>
-    </div>
-@else
-    <p>You are already logged in!</p>
-@endif
+<div class="userForm">
+    <form action="{{ url('/two-factor-challenge') }}" method="POST">
+        @csrf
+        <label for ="code">Two Factor Code:</label>
+        <input type="text" name="code" id="code"/>
+        <p style="font-size: 14px;"><a href="{{ route('recoveryLogin') }}">Don't have access to authenticator?</a></p>
+        <button type="submit" class="btn btn-blue">Submit</button>
+
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <p>{{ $error }}</p>
+            @endforeach
+        @endif
+    </form>
+</div>
