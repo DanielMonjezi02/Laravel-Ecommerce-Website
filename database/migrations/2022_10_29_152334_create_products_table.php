c17fdc1f6dd83591f4bfc80c3e163ae0237ce275
Daniel Monjezi <U2066800@unimail.hud.ac.uk>
2022-12-04 17:05:43 +0000

    Changed product price to float

--- database/migrations/2022_10_29_152334_create_products_table.php
+++ database/migrations/2022_10_29_152334_create_products_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->float('price');
+            $table->decimal('price', 6, 2);
             $table->integer('quantity');
             $table->text('description');
             $table->foreignIdFor(Category::class);
