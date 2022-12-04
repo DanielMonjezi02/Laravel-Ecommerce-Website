9a0a7d8fbd24e0a09f927e1c5cff3b2a02f1aa12
Daniel Monjezi <U2066800@unimail.hud.ac.uk>
2022-12-04 16:58:55 +0000

    Added quantity column in products database

--- database/migrations/2022_10_29_152334_create_products_table.php
+++ database/migrations/2022_10_29_152334_create_products_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->float('price');
+            $table->integer('quantity');
             $table->text('description');
             $table->foreignIdFor(Category::class);
             $table->timestamps();
