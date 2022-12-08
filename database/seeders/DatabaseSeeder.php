375906607f9aa7159e3cdfb6f5222f5d2acaa63b
Daniel Monjezi <U2066800@unimail.hud.ac.uk>
2022-12-08 16:06:36 +0000

    Created coupon model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Database\Seeders\CategorySeeder;
 
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +11,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([CategorySeeder::class]); // Calling category seeder so it creates the category and products 
         $this->call([UserSeeder::class]); // Calling user seeder to create a temporary user that can be used for testing 
+        $this->call([CouponSeeder::class]);
     }
 }
