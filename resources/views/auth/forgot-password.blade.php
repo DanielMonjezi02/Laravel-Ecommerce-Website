db25b72f85891a3ff64f3add45fe06231b45ab11
Daniel Monjezi <U2066800@unimail.hud.ac.uk>
2022-12-14 09:56:30 +0000

    Created a password reset table

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -2,6 +2,9 @@
 @section('title', 'Reset Password')
 
 <div class="userForm">
+    @if(session('status'))
+        <div class="alert alert-danger">{{ session('status') }}</div>
+    @endif
     <form action="{{ route('password.request') }}" method="POST">
         @csrf
         <label for="email">Email:</label>
