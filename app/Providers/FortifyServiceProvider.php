e42a57c0becd03eba39d9a6db8b580d1767c6835
Daniel Monjezi <U2066800@unimail.hud.ac.uk>
2022-12-14 09:51:12 +0000

    Created blade view for forgot password

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -62,5 +62,9 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::confirmPasswordView(function () {
             return view('auth.confirm-password');
         });
+
+        Fortify::requestPasswordResetLinkView(function(){
+            return view('auth.forgot-password');
+        });
     }
 }
