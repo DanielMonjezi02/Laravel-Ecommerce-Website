edd3c8fa7306f9dab35fd643e76f50fcb45cd06a
Daniel Monjezi <U2066800@unimail.hud.ac.uk>
2022-12-14 10:05:23 +0000

    Created a blade view to reset the password

---
+++ resources/views/auth/reset-password.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('title', 'Reset Password')
+
+<div class="userForm">
+    <form action="{{ route('password.update') }}" method="POST">
+        @csrf
+        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email" class="p-2 bg-gray-200 @error('email') is-invalid @enderror" value="{{ $request->email }}" />
+        @error('email')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        <label for="password">Password:</label>
+        <input name="password" id="password" class="p-2 bg-gray-200 @error('password') is-invalid @enderror" />
+        @error('password')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        <label for="password_confirmation">Password Confirm:</label>
+        <input name="password_confirmation" id="password_confirmation" class="p-2 bg-gray-200 @error('password_confirmation') is-invalid @enderror" />
+        @error('password_confirmation')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        <button type="submit" class="btn btn-blue">Update</button>
+    </form>
+</div>
