3d791bddca762bad3718b75c6f92fe2e9bfa8c96
Daniel Monjezi <U2066800@unimail.hud.ac.uk>
2022-12-16 23:08:07 +0000

    Removed quantity column in products
    Quantity is not neccassery to have in products table

--- database/migrations/2022_10_29_152334_create_products_table.php
+++ database/migrations/2022_10_29_152334_create_products_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->decimal('price', 6, 2);
-            $table->integer('quantity');
             $table->text('description');
             $table->timestamps();
 
